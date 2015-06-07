80de3dfae18b1ddb85d5ae20f015f2c71ce12220
Tatyana <tatyana.simonenko@gmail.com>
2015-06-07 13:10:59 +0400

    why here, desktop

--- gulpfile.js
+++ gulpfile.js
@@ -185,8 +185,8 @@ gulp.task( 'deploy', function() {
 
   var conn = ftp.create( {
       host: 'a.epixx.ru',
-      user: '',
-      password: '',
+      user: 'epic_assets',
+      password: '1TGHryqndKUXNtxnWO0u8JcpTVVS29',
       parallel: 10,
       port: 21,
       log: gutil.log
