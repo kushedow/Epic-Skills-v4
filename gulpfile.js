7a4438535da77792c6a5cdbabda54f40b902eeb6
100lp <spb.gurme@gmail.com>
2015-06-11 21:02:19 +0300

    Footer::add backgrounds for mobile devices

--- gulpfile.js
+++ gulpfile.js
@@ -184,9 +184,10 @@ gulp.task('build', ['clean', 'jade'], function () {
 gulp.task( 'deploy', function() {
 
   var conn = ftp.create( {
-      host: 'mediaflowers.ru',
-      user: 'fr79069n',
-      password: 'bD7DrzTa',
+      host: '',
+      user: '',
+      password: '',
+      port: 20,
       parallel: 10,
       log: gutil.log
   } );
@@ -196,5 +197,5 @@ gulp.task( 'deploy', function() {
   ];
 
   return gulp.src(globs, { base: 'dist/', buffer: false })
-    .pipe(conn.dest( 'fr79069n.bget.ru/public_html/'));
+    .pipe(conn.dest( 'public_html/v4/'));
 });
